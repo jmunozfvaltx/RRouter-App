8ce6c0332651d0cb454cdc71d950cf84cdaabbae
jlmunozfdev <jlmunozf.dev@gmail.com>
2023-12-12 17:17:36 -0500

    Add models, migrations and update: resultado y views

--- devices/models.py
+++ devices/models.py
@@ -1,3 +1,20 @@
+# tu_aplicacion/models.py
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+    ip = models.GenericIPAddressField()
+    hostname = models.CharField(max_length=255)
+    syslog = models.CharField(max_length=255)
+    web_access = models.CharField(max_length=255)
+    ssh = models.CharField(max_length=255)
+    snmp = models.CharField(max_length=255)
+    users = models.CharField(max_length=255)
+    ntp = models.CharField(max_length=255)
+    no_telnet = models.CharField(max_length=255)
+    inactivity = models.CharField(max_length=255)
+    interfaces = models.CharField(max_length=255)
+    protocols = models.CharField(max_length=255)
+    policies = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.hostname
--- devices/views.py
+++ devices/views.py
@@ -2,8 +2,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .utils import Verification  # Define esta función en un archivo utils.py
+from .models import Device #importa el modelo Device
 
 def index(request):
+    
+    devices = Device.objects.all()
+    
     if request.method == 'POST':
         # Obtén los datos del formulario
         user = request.POST.get('user')
@@ -12,9 +16,31 @@ def index(request):
 
         # Realiza la verificación de dispositivos (usa la función que defines en utils.py)
         resultados = Verification(user, password, ruta)
+        
+        # Guarda los resultados en la base de datos
+        for resultado in resultados:
+            device = Device(
+                ip=resultado['ip'],
+                hostname=resultado['hostname'],
+                syslog=resultado['syslog'],
+                web_access=resultado['web_access'],
+                ssh=resultado['ssh'],
+                snmp=resultado['snmp'],
+                users=resultado['users'],
+                ntp=resultado['ntp'],
+                no_telnet=resultado['no_telnet'],
+                inactivity=resultado['inactivity'],
+                interfaces=resultado['interfaces'],
+                protocols=resultado['protocols'],
+                policies=resultado['policies']
+            )
+            device.save()
+            
+        # Actualiza la lista de dispositivos después de guardar en la base de datos
+        devices = Device.objects.all()
 
-        # Pasa los resultados a la plantilla
-        return render(request, 'resultado.html', {'resultados': resultados})
+        # # Pasa los resultados a la plantilla
+        # return render(request, 'resultado.html', {'resultados': device})
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'devices' : devices})
 
